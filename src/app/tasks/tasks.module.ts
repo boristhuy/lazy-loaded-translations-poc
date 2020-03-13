c3c6e7998e8dd43b6afcf4aebfbbc0254f6d9e95
Boris Thuy <boristhuy@gmail.com>
2020-03-13 17:59:34 +0100

    Replace PrimeNg with Angular Material

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CardModule} from 'primeng/card';
 import { TasksComponent } from './tasks.component';
 import {TasksRoutingModule} from './tasks-routing.module';
 
 @NgModule({
   declarations: [TasksComponent],
   imports: [
-    CardModule,
     CommonModule,
     TasksRoutingModule
   ]
